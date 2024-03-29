6160bcaee01732c3a6eb4eeb87935c46a9dd1cec
Christopher Holzmann Pérez <christopherholzmannperez@gmail.com>
2024-03-29 04:47:53 +0100

    Add context menu to the root directory TreeView

--- Golden Axe/CDIExplorer/CDIExplorerUC.xaml.cs
+++ Golden Axe/CDIExplorer/CDIExplorerUC.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using AceUtils.CDI;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Golden_Axe.CDIExplorer
 {
@@ -14,6 +16,8 @@ namespace Golden_Axe.CDIExplorer
         /// The REGFILE.CDI to display.
         /// </summary>
         internal CDI REGFILE;
+
+        CDIExplorerDirectoryUC explorer;
 
         public CDIExplorerUC(CDI regfile)
         {
@@ -33,12 +37,14 @@ namespace Golden_Axe.CDIExplorer
                 CDIFolderItemTreeView tvfolder = new CDIFolderItemTreeView()
                 {
                     Name = folder.Name,
+                    Folder = folder,
                 };
                 foreach(CDIFile file in folder.GetFiles())
                 {
                     CDIFileItemTreeView tvfile = new CDIFileItemTreeView()
                     {
                         Name = file.Name,
+                        File = file,
                     };
                     tvfolder.Files.Add(tvfile);
                 }
@@ -52,9 +58,82 @@ namespace Golden_Axe.CDIExplorer
 
         private void InitDirectoryExplorer()
         {
-            CDIExplorerDirectoryUC explorer = new CDIExplorerDirectoryUC(this);
+            explorer = new CDIExplorerDirectoryUC(this);
             grid_DirectoryExplorer.Children.Add(explorer);
             explorer.DisplayDirectory(REGFILE.GetFolders());
+        }
+
+
+        private void mi_tv_FolderOpen_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem treeViewItem = GetTreeViewItemFromMenuItem(sender);
+
+            if (treeViewItem != null)
+            {
+                CDIFolderItemTreeView folderItem = treeViewItem.Header as CDIFolderItemTreeView;
+                if (folderItem != null && explorer != null)
+                {
+                    explorer.DisplayDirectory(folderItem.Folder.GetFiles(), folderItem.Name);
+                }
+            }
+        }
+
+
+        private void mi_tv_FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem treeViewItem = GetTreeViewItemFromMenuItem(sender);
+
+            if (treeViewItem != null)
+            {
+                CDIFileItemTreeView fileItem = treeViewItem.Header as CDIFileItemTreeView;
+                if (fileItem != null)
+                {
+                    FileEditorHandler.OpenFileEditor(fileItem.File);
+                }
+            }
+        }
+
+
+        private void StackPanel_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            TreeViewItem treeViewItem = GetTreeViewItemFromStackPanel(sender);
+
+            if (treeViewItem != null)
+            {
+                treeViewItem.IsSelected = true;
+            }
+        }
+
+
+        private TreeViewItem GetTreeViewItemFromMenuItem(object sender)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            ContextMenu contextMenu = menuItem.Parent as ContextMenu;
+            StackPanel stackPanel = contextMenu.PlacementTarget as StackPanel;
+            TreeViewItem treeViewItem = FindAncestor<TreeViewItem>(stackPanel);
+            return treeViewItem;
+        }
+
+
+        private TreeViewItem GetTreeViewItemFromStackPanel(object sender)
+        {
+            StackPanel stackPanel = sender as StackPanel;
+            TreeViewItem treeViewItem = FindAncestor<TreeViewItem>(stackPanel);
+            return treeViewItem;
+        }
+
+
+        private T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            } while (current != null);
+            return null;
         }
     }
 
@@ -68,6 +147,8 @@ namespace Golden_Axe.CDIExplorer
 
         public string Name { get; set; }
 
+        public CDIFolder Folder { get; set; }
+
         public ObservableCollection<CDIFileItemTreeView> Files { get; set; }
     }
 
@@ -75,6 +156,8 @@ namespace Golden_Axe.CDIExplorer
     public class CDIFileItemTreeView
     {
         public string Name { get; set; }
+
+        public CDIFile File { get; set; }
     }
 
 }
